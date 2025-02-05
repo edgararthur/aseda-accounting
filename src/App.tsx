7c1230f7b8ae2997e1c6c15562f451d12c4c90e0
edgararthur <gilfoyle.arthur@protonmail.com>
2025-02-05 14:50:27 +0000

    removed redundant imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { router } from '@/lib/routes';
-import { Toaster } from '@/components/ui/toaster';
+// import { QueryClient } from '@tanstack/react-query';
+// import { router } from '@/lib/routes';
+// import { Toaster } from '@/components/ui/toaster';
 import { Dashboard } from './components/dashboard/Dashboard';
 import { FinancialOverview } from './components/dashboard/FinancialOverview';
 import { Sales } from './components/sales/Sales'
 import { Invoice } from './components/dashboard/Invoice';
 import { InvoicingBilling } from './components/InvoicingBilling';
-import { Tax } from './components/tax/Tax';
+// import { Tax } from './components/tax/Tax';
 import { Ledger } from './components/ledger/Ledger';
 import { Payables } from './components/payables/Payables';
 import TaxManagement from './components/tax/TaxManagment';
 import ReceiptsReport from './components/reports/RecieptsReport';
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 export default function App() {
   return (
